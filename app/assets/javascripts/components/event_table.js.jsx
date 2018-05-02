e20566e1c2e26f7656ccc83af86119c1d89e8885
huongnguyenmta <huongnt0809@gmail.com>
2018-05-02 20:18:50 +0700

    init project

---
+++ app/assets/javascripts/components/event_table.js.jsx
@@ -0,0 +1,19 @@
+var createReactClass = require('create-react-class');
+
+var EventTable = createReactClass ({
+  render: function() {
+    return(
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th className="col-md-3">Name</th>
+            <th className="col-md-2">Date</th>
+            <th className="col-md-3">Place</th>
+            <th className="col-md-4">Description</th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
+    )
+  }
+});
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root "dashboard#index"
+
+  namespace :api do
+    resources :events
+  end
+end
